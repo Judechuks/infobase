931e1dac04d3d44e79fe3b2675830f7b9e96b0d8
Jude Chuks <judechuks26@gmail.com>
2024-05-15 20:07:48 +0100

    recentblog section and its styles

---
+++ src/sections/recentblog/RecentBlog.jsx
@@ -0,0 +1,27 @@
+import { Blog } from "../../components";
+import { Link } from "react-router-dom";
+// import { blogData } from "../../constant";
+import { useGlobalContext } from "../../context";
+import "./RecentBlog.scss";
+
+const RecentBlog = () => {
+  const { blogData } = useGlobalContext();
+  return (
+    <section id="recent">
+      <h1 className="section-title">Recent Blogs</h1>
+      <article>
+        {
+          blogData?.data
+            ?.map((blog, index) => (
+              <Blog key={index} id={blog.id} {...blog?.attributes} />
+            ))
+            .slice(-3) // last 3 events
+        }
+      </article>
+      <Link to="blog" className="btn see-more">
+        More blogs
+      </Link>
+    </section>
+  );
+};
+export default RecentBlog;
