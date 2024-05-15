16ec5c4ca7ccedf9045e5554d8bf40cb01a13725
Jude Chuks <judechuks26@gmail.com>
2024-05-15 20:09:12 +0100

    about section and its styles

---
+++ src/sections/about/About.jsx
@@ -0,0 +1,29 @@
+// import { aboutData } from "../../constant";
+import { useGlobalContext } from "../../context";
+import "./About.scss";
+
+const About = () => {
+  const { aboutData, domain } = useGlobalContext();
+  return (
+    <section id="about">
+      <h1 className="section-title">
+        {aboutData?.data?.attributes.aboutTitle}
+      </h1>
+      <div className="about-container">
+        <article>
+          <img
+            src={`${domain}${aboutData?.data?.attributes.aboutImg.data?.attributes?.formats?.large?.url}`}
+            alt="hero image"
+          />
+        </article>
+        <article>
+          <p>{aboutData?.data?.attributes.aboutDesc}</p>
+          <a href="/about" className="btn see-more">
+            Read more
+          </a>
+        </article>
+      </div>
+    </section>
+  );
+};
+export default About;
