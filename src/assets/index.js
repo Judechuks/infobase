d7b75a5345bdf9346dea068f1161e45b49e4ca0a
Jude Chuks <judechuks26@gmail.com>
2024-05-17 19:56:13 +0100

    favicon image

---
+++ src/assets/index.js
@@ -0,0 +1,29 @@
+import blog1 from "../assets/blog-01.jpg";
+import blog2 from "../assets/blog-02.jpg";
+import blog3 from "../assets/blog-03.jpg";
+import event1 from "../assets/event-01.jpg";
+import event2 from "../assets/event-02.jpg";
+import event3 from "../assets/event-03.jpg";
+import team1 from "../assets/team-1.jpg";
+import team2 from "../assets/team-2.jpg";
+import team3 from "../assets/team-3.jpg";
+import team4 from "../assets/team-4.jpg";
+import drums from "../assets/drums.jpg";
+import logo from "../assets/logo.jpg";
+import heroImg from "../assets/heroImg.jpg";
+
+export {
+  blog1,
+  blog2,
+  blog3,
+  event1,
+  event2,
+  event3,
+  team1,
+  team2,
+  team3,
+  team4,
+  drums,
+  logo,
+  heroImg,
+};
