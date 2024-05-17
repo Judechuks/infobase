f365b0b61df3b21fe55782b5350761dd3816aad5
Jude Chuks <judechuks26@gmail.com>
2024-05-17 19:55:03 +0100

    list of exported sections

---
+++ src/sections/index.js
@@ -0,0 +1,8 @@
+export { default as Header } from "./header/Header";
+export { default as Hero } from "./hero/Hero";
+export { default as About } from "./about/About";
+export { default as RecentBlog } from "./recentblog/RecentBlog";
+export { default as UpcomingEvents } from "./upcoming/UpcomingEvents";
+export { default as NewsLetter } from "./newsletter/NewsLetter";
+export { default as Testimonials } from "./testimonial/Testimonials";
+export { default as Footer } from "./footer/Footer";
