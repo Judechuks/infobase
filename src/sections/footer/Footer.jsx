d131687f49ad372f69d05170453bdc9b5bcaa03d
Jude Chuks <judechuks26@gmail.com>
2024-05-18 16:18:52 +0100

    changed the about url

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -32,10 +32,10 @@ const Footer = () => {
           <h2>Quick Links</h2>
           <ul>
             <li>
-              <a href="/#">Home</a>
+              <a href="/">Home</a>
             </li>
             <li>
-              <a href="/#about">About</a>
+              <a href="/about">About</a>
             </li>
             <li>
               <a href="/contact">Contact</a>
