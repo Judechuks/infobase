0651dec8fe692564d0b464c0279f71e4946ebd64
Jude Chuks <judechuks26@gmail.com>
2024-05-18 15:53:20 +0100

    exported pages

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as Missing } from "./missing/Missing";
+export { default as EventListPage } from "./eventList/EventListPage";
+export { default as BlogListPage } from "./blogList/BlogListPage";
+export { default as EventPage } from "./event/EventPage";
+export { default as BlogPage } from "./blog/BlogPage";
+export { default as Contact } from "./contact/Contact";
+export { default as AboutUs } from "./about/AboutUs";
