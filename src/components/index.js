7e4fdc2d26eab552003796446236af4fb9f5e2a4
Jude Chuks <judechuks26@gmail.com>
2024-05-19 12:34:41 +0100

    exported components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Blog } from "./blog/Blog";
+export { default as Event } from "./event/Event";
+export { default as CountDownTimer } from "./countdown/CountDownTimer";
+export { default as ModalOverlay } from "./modal/ModalOverlay";
+export { default as Spinner } from "./spinner/Spinner";
+export { default as TestimonialCard } from "./card/TestimonialCard";
