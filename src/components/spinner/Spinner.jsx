03e597334dfcb01da7284b8590fd24fcef694ee4
Jude Chuks <judechuks26@gmail.com>
2024-05-19 12:34:10 +0100

    spinner component and some styles

---
+++ src/components/spinner/Spinner.jsx
@@ -0,0 +1,21 @@
+import { Circles } from "react-loader-spinner";
+import "./Spinner.scss";
+
+const Spinner = ({ message }) => {
+  return (
+    <main className="spinner">
+      <div className="loader-container">
+        <Circles color="#00BFFF" height={80} width={200} className="loader" />
+        <p className="message">{message}</p>
+      </div>
+    </main>
+  );
+};
+export default Spinner;
+
+/* Where we have Spinners like;
+<Audio /> <Circles /> <Rings /> etc 
+to see the others, visit the node modules folder, navigate to the "react-loader-spinner" folder and open the main.js file you will see
+$parcel$export(module.exports, "Audio", () => $b6f4e6a49a3349b1$export$153755f98d9861de);
+with the different type of spinner being exported.
+*/
