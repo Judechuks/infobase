86240f5dd2d22e9a7017c2a6dcebc25f7b6ec4ca
Jude Chuks <judechuks26@gmail.com>
2024-05-19 12:33:39 +0100

    modal component and its styles

---
+++ src/components/modal/ModalOverlay.jsx
@@ -0,0 +1,56 @@
+import { RiCloseFill } from "react-icons/ri";
+import { useGlobalContext } from "../../context";
+import "./ModalOverlay.scss";
+
+const ModalOverlay = ({ index, setIndex, itemOnModal, setShowModal }) => {
+  const { domain } = useGlobalContext();
+
+  const prevSlide = (index) => {
+    if (index == 0) {
+      setIndex(itemOnModal.length - 1);
+    } else {
+      setIndex(index - 1);
+    }
+  };
+  const nextSlide = (index) => {
+    if (index == itemOnModal.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  };
+
+  return (
+    <section className="modal-container">
+      <article className="modal">
+        <button className="close-btn" onClick={() => setShowModal(false)}>
+          <RiCloseFill />
+        </button>
+        <div className="image-container">
+          {
+            itemOnModal?.map((item, itemIndex) => (
+              <img
+                key={itemIndex}
+                src={`${domain}${item.attributes.url}`}
+                alt="photo gallery"
+              />
+            ))[index] // to get just one of the pictures at a time
+          }
+        </div>
+        <div className="btn-container">
+          <button
+            className="modal-btn prev-btn"
+            onClick={() => prevSlide(index)}>
+            {"<"}
+          </button>
+          <button
+            className="modal-btn next-btn"
+            onClick={() => nextSlide(index)}>
+            {">"}
+          </button>
+        </div>
+      </article>
+    </section>
+  );
+};
+export default ModalOverlay;
